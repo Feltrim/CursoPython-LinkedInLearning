805cbafff2750ceb85945af9a69beb24402952b6
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-05-19 19:45:48 -0300

    Cap. 04

--- arquivos_de_exercicios_descubra_o_python/Cap. 04/leituraArquivo_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/leituraArquivo_start.py
@@ -1,3 +1,27 @@
 #
 # Lendo arquivos com funções do Python
 #
+
+def leitura_arquivo():
+    arquivo = open("novo_arquivo.txt", "r")
+    if arquivo.mode == "r":
+        conteudo = arquivo.read()
+        print(conteudo)
+    arquivo.close()
+
+
+leitura_arquivo()
+
+
+def leitura_arquivo_grande():
+    arquivo = open("novo_arquivo.txt", "r")
+    if arquivo.mode == "r":
+        conteudo_total = arquivo.readlines()
+
+        for conteudo in conteudo_total:
+            print(conteudo)
+
+    arquivo.close()
+
+
+leitura_arquivo_grande()
--- arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
@@ -2,8 +2,30 @@
 # Exemplos de como trabalhar com arquivos
 #
 import os
-from os import path
 import shutil
-from shutil import make_archive
 from zipfile import ZipFile
 
+
+def cria_zip_modo1():
+    shutil.make_archive("arquivo_compactado", "zip",
+                        "C:\\Users\\Feltrim\\OneDrive\\Documentos\\GitHub\\CursoPython-LinkedInLearning\\arquivos_de_exercicios_descubra_o_python\\Cap. 03")
+
+
+cria_zip_modo1()
+
+
+def cria_zip_modo2():
+    with ZipFile("arquivo_zip_modo2.zip", "w") as novoZip:
+        novoZip.write("arquivo_alterado.txt")
+        novoZip.write("novo_arquivo.txt")
+        novoZip.write("arquivo_compactado.zip")
+
+
+cria_zip_modo2()
+
+
+def deleta_arquivo():
+    os.remove("arquivo_zip_modo2.zip")
+
+
+deleta_arquivo()
