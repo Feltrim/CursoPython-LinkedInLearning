62cfabbdbebbad9853b39c0f39d6aadb72ce8d5d
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-05-20 21:05:30 -0300

    Cap. 06

--- arquivos_de_exercicios_descubra_o_python/Cap. 06/buscarDadosMongoDB_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 06/buscarDadosMongoDB_start.py
@@ -1,23 +1,25 @@
-# 
+#
 # Exemplo de acesso a uma base de dados SQLite
 #
 import pymongo
 from pymongo import MongoClient
 
-def manipulaDadosMongoDB():
+
+def manipula_dados_mongodb():
     cliente = pymongo.MongoClient("mongodb://localhost:27017")
     db = cliente.conheca_python
 
-    for i in range(1,10):
-        objDic = {'codigo' : i}
+    for i in range(1, 10):
+        objDic = {'codigo': i}
         db.conheca_mongodb.insert_one(objDic)
 
-    db.conheca_mongodb.update_one({'codigo' : 2}, {"$set" : {'atributoNovo' : 789}})   
-    db.conheca_mongodb.delete_one({'codigo' : 1})
+    db.conheca_mongodb.update_one(
+        {'codigo': 2}, {"$set": {'atributoNovo': 789}})
+    db.conheca_mongodb.delete_one({'codigo': 1})
 
-    resultadoConsulta = db.conheca_mongodb.find({})
-    for doc in resultadoConsulta:
-        print (doc)
+    resultado_consulta = db.conheca_mongodb.find({})
+    for doc in resultado_consulta:
+        print(doc)
 
-manipulaDadosMongoDB()
 
+manipula_dados_mongodb()
--- arquivos_de_exercicios_descubra_o_python/Cap. 06/buscarDadosSQLite_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 06/buscarDadosSQLite_start.py
@@ -1,6 +1,35 @@
-# 
+#
 # Exemplo de acesso a uma base de dados SQLite
 #
 
 import sqlite3
 
+
+def manipula_dados(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+
+
+def seleciona_dados(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    linhas = cursor.fetchall()
+
+    for linha in linhas:
+        print(linha)
+
+
+def manipulacao_dados():
+    comandoInsert = "INSERT INTO estados (nome_estado, sigla_estado, nome_capital) VALUES ('Estado Teste', 'XX', 'Teste Inclusão')"
+    pathBD = "BancoDeDados.db"
+    comandoSELECT = "SELECT nome_estado, sigla_estado, nome_capital FROM estados"
+
+    manipula_dados(pathBD, comandoInsert)
+    seleciona_dados(pathBD, comandoSELECT)
+
+
+manipulacao_dados()
